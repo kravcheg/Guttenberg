d6379f6ec90e73797366eeed130027d36210f4ea
Sasha Kravchik <alex.kravchik@sap.com>
2014-10-17 22:17:23 +0300

    rdf parser

--- node.js/db/lib/.rdf-parser.js
+++ node.js/db/lib/.rdf-parser.js
@@ -1,3 +1,28 @@
-const 
+'use strict';
+
+const
 fs = require("fs"),
-cheerio = require("cheerio) ;
+cheerio = require("cheerio") ;
+
+module.exports =
+    function (file, callback){
+        fs.readFile (filename ,  function (err, data){
+            if (err) {
+                return callback (err) ;
+            }
+            var
+                $ = cheerio.load(data.toString()) ,
+                collect = function (index , elem ){
+                    return  $ (elem).text();
+                };
+
+            callback(null, {
+                 _id: $('pgterms\\:ebook').attr('rdf:about').replace('ebooks/', ''),
+                 title: $('dcterms\\:title').text(),
+                 authors: $('pgterms\\:agent pgterms\\:name').map(collect),
+                 subjects: $('[rdf\\:resource$="/LCSH"] ~ rdf\\:value').map(collect)
+
+                }
+            );
+    });
+  };
