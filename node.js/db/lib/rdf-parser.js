cb4310f28855bdd2fea2cebd95171e934780c480
Sasha Kravchik <alex.kravchik@sap.com>
2014-10-17 23:37:25 +0300

    rdf parser

--- node.js/db/lib/rdf-parser.js
+++ node.js/db/lib/rdf-parser.js
@@ -1,28 +1,42 @@
 'use strict';
 
-const
-fs = require("fs"),
-cheerio = require("cheerio") ;
+const fs = require('fs'),
+    cheerio = require('cheerio');
 
-module.exports =
-    function (filename, callback){
-        fs.readFile (filename ,  function (err, data){
-            if (err) {
-                return callback (err) ;
-            }
-            var
-                $ = cheerio.load(data.toString()) ,
-                collect = function (index , elem ){
-                    return  $ (elem).text();
-                };
+module.exports = function(filename, callback) {
+    fs.readFile(filename, function(err, data) {
+        if(err) { return callback(err); }
 
-            callback(null, {
-                 _id: $('pgterms\\:ebook').attr('rdf:about').replace('ebooks/', ''),
-                 title: $('dcterms\\:title').text(),
-                 authors: $('pgterms\\:agent pgterms\\:name').map(collect),
-                 subjects: $('[rdf\\:resource$="/LCSH"] ~ rdf\\:value').map(collect)
+        var $ = cheerio.load(data.toString()),
+            _map = function(items, mapper) {
+                var rv = [];
 
-                }
-            );
+                items.each(function(i, elem) {
+                    rv.push(mapper(elem));
+                });
+
+                return rv;
+            },
+            collect = function(index, elem) {
+                var rv = $(elem).text();
+                return rv;
+            },
+            authors = $('pgterms\\:agent pgterms\\:name'),
+            subjects = $('[rdf\\:resource$="/LCSH"]');
+        var doc = {
+            _id: $('pgterms\\:ebook').attr('rdf:about').replace('ebooks/', ''),
+            title: $('dcterms\\:title').text(),
+            authors: _map(authors, function(elem) {
+                return $(elem).text();
+            }),
+            subjects: _map(subjects, function(elem) {
+                return $(elem).find('rdf\\:value').text();
+            })
+        };
+
+        if(doc._id === undefined)
+            callback("something went wrong");
+        else
+            callback(null, doc);
     });
-  };
+};
