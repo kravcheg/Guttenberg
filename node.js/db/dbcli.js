c1213aa6ffdb0e3692db5343517f823536e35b5b
Ubuntu <ubuntu@ip-172-31-18-45.us-west-2.compute.internal>
2014-10-17 18:55:41 +0000

    db client as bash script + change couchdb ip to localhost

--- node.js/db/dbcli.js
+++ node.js/db/dbcli.js
@@ -1,10 +1,12 @@
-#! node --harmony
+#!/bin/sh
+':' //; exec "$(command -v nodejs || command -v no^Z^Z^Z)" "$0" "$@"
+
 
 const
     request = require('request'),
     options = {
         method: process.argv[2] || 'GET',
-        url: 'http://admin:abc123@54.69.67.10:5984/' + (process.argv[3] || '')
+        url: 'http://admin:abc123@localhost:5984/' + (process.argv[3] || '')
     };
 request(options, function(err, res, body) {
     if (err) {
