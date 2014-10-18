422082f0ee6ec2198ada27251d3b6fa70c6aaa6e
Sasha Kravchik <alex.kravchik@sap.com>
2014-10-18 23:09:29 +0300

    prepare views in couchdb

--- node.js/db/make-views.js
+++ node.js/db/make-views.js
@@ -1,52 +1,38 @@
-#!/bin/sh
-':' //; exec "$(command -v nodejs || command -v no^Z^Z^Z)" "$0" "$@"
+#!/usr/bin/env node --harmony
+'use strict';
 
-'use strict' ;
+const async = require('async'),
+    request = require('request'),
+    views = require('./lib/views.js');
 
-const
-    async  = require ('async'),
-    request = require ('request'),
-    views = ('./lib/views.js');
+async.waterfall([
 
-
-async.waterfall (
-    [
-    // get the existing design doc (if present)
+    //get the existing design doc, if present
     function(next) {
-        request.get('http://localhost:5984/books/_design/books', next);
+        request.get('http://admin:123@localhost:5984/books/_design/books', next);
     },
 
-    // create a new design doc or use existing
+    //create a new design or use existing
     function(res, body, next) {
-        if (res.statusCode === 200) {
+        if(res.statusCode === 200) {
             next(null, JSON.parse(body));
-        } else if (res.statusCode === 404) {
-            next(null, { views: {} });
+        } else if(res.statusCode === 404) {
+            next(null, { views: {}});
         }
     },
 
-    // add views to document and submit
     function(doc, next) {
         Object.keys(views).forEach(function(name) {
             doc.views[name] = views[name];
         });
+
         request({
             method: 'PUT',
-            url: 'http://localhost:5984/books/_design/books',
+            url: 'http://admin:123@localhost:5984/books/_design/books',
             json: doc
         }, next);
     }
-    ],
-    function (err, res , body ){
-        if (err) {
-            throw err;
-        }
-        console.log (res.statusCode, body) ;
-    }
-
-);
-
-
-/**
- * Created by I048692 on 10/18/14.
- */
+], function(err, res, body) {
+    if(err) throw err;
+    console.log(res.statusCode, body);
+});
