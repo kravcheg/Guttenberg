6bd306fd14b5258cd55258bd040174102bfcbec2
Sasha Kravchik <alex.kravchik@sap.com>
2014-12-13 22:05:49 +0200

    web services express

--- node.js/web-services/hello/server.js
+++ node.js/web-services/hello/server.js
@@ -1,14 +1,17 @@
 'use strict';
 const
     express = require('express'),
+    morgan = require('morgan'),
+   // logger = morgan('combined'),
     app = express();
 
 
    /// app.use(express.logger('dev'));
 
-
+    app.use(morgan('combined'));
     app.get('/api/:name', function(req, res) {
-        res.json(200, { "hello": req.params.name });
+        //res.json(200, { "hello": req.params.name });
+        res.status(200).json({ "preved": req.params.name });
     });
     app.listen(3000, function(){
         console.log("express ready captain.");
